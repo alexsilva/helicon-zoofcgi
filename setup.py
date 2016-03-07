da6c6bb12342ed4a7969598d56444030c30cfa34
Alex Sandro <geniofuturo@gmail.com>
2016-03-07 10:20:45 -0300

    Script setup for installation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='helicon-zoofcgi',
+    version='1.0',
+    py_modules =['zoofcgi'],
+    url='https://github.com/alexsilva/helicon-zoofcgi',
+    license='The MIT License (MIT)',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='FCGI module with fixes that let you perform a Django project, within an app in a prefix.'
+)
